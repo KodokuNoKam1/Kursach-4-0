30f35facb8fd689ef5c66159d0bed4a0ccb72256
KodokuNoKam1 <153433903+KodokuNoKam1@users.noreply.github.com>
2025-04-29 17:09:00 +0300

    backend start

---
+++ backend/commercial-offer/src/main/java/by/bsuir/kursach/commercialoffer/CommercialOfferApplication.java
@@ -0,0 +1,13 @@
+package by.bsuir.kursach.commercialoffer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommercialOfferApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommercialOfferApplication.class, args);
+    }
+
+}
---
+++ backend/commercial-offer/src/test/java/by/bsuir/kursach/commercialoffer/CommercialOfferApplicationTests.java
@@ -0,0 +1,13 @@
+package by.bsuir.kursach.commercialoffer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommercialOfferApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
